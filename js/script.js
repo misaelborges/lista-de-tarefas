07f52f82616fca08ef62df1575416bdb34faab7f
misael borges cancelier <mizaelborges2011@gmail.com>
2022-09-19 17:58:06 -0300

    adicionado a função de colocar horário nas tarefas, e inicio das funções dos botões

--- js/script.js
+++ js/script.js
@@ -1,22 +1,55 @@
 let botao = document.querySelector('#botao')
+
 botao.addEventListener('click', function(){
+
     let formulario = document.querySelector('#form-atividade')
 
-    let nome = formulario.nome.value 
+    let atividade = listaTarefas(formulario)
 
-    if (nome == "") {
+    if (atividade.nome == "") {
         window.alert('campo em branco')
     } else {
-        let atividadeTr = document.createElement('tr')
-
-        let atividadeTd = document.createElement('td')
-
-        atividadeTd.textContent = nome
-
-        atividadeTr.appendChild(atividadeTd)
+        let atividadeTr = criarTr(atividade)
 
         let tabela = document.querySelector('#compromisso')
 
         tabela.appendChild(atividadeTr)
+
+        formulario.reset()
     }    
 })
+
+function listaTarefas(dados) {
+    let atividade = {
+        nome: dados.nome.value, 
+        hora: dados.hora.value,
+        situacao: criarBotao()
+    }
+    return atividade
+}
+
+function criarTr(atividade) {
+    let atividadeTr = document.createElement('tr')
+
+    atividadeTr.appendChild(criarTd(atividade.nome))
+    atividadeTr.appendChild(criarTd(atividade.hora))
+    atividadeTr.appendChild(criarTd(atividade.situacao))
+
+    return atividadeTr
+}
+
+function criarTd(dado) {
+    var td = document.createElement('td')
+    td.textContent = dado
+
+    return td
+}
+
+function criarBotao() {
+    let botao = {
+        btnConcluir: document.createElement('input'),
+        btnAdiar: document.createElement('input'),
+        btnExcluir: document.createElement('input')
+    }
+    return botao
+}
